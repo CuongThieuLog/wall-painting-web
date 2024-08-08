e9b31a513504d6c6b34b9b8f141db10e4a413cb2
CuongThieu <cuongtt@solashi.com>
2024-08-08 16:35:11 +0700

    Config firebase

--- src/lib/firebase/firebaseConfig.ts
+++ src/lib/firebase/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoGbiU6aTHj2nMSbsSaTrIpMGDj_Niz5I",
-  authDomain: "panting-9c670.firebaseapp.com",
-  projectId: "panting-9c670",
-  storageBucket: "panting-9c670.appspot.com",
-  messagingSenderId: "699829832022",
-  appId: "1:699829832022:web:0b80b782d0d5d3cfd0aef7",
-  measurementId: "G-BL8PWBEQZ9",
+  apiKey: "AIzaSyB3TmVBi6Vdg3n9jj0hwT62e0v5pd-r6TU",
+  authDomain: "wall-painting-745a0.firebaseapp.com",
+  projectId: "wall-painting-745a0",
+  storageBucket: "wall-painting-745a0.appspot.com",
+  messagingSenderId: "562195902865",
+  appId: "1:562195902865:web:ebacfb5e91564a89bbaa30",
+  measurementId: "G-31N9ZRXTV4",
 };
 
 const app = initializeApp(firebaseConfig);
